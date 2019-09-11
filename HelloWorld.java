1e277a66bea7c8d9e47e08ceaa72ed5c344d8494
MUYI-12 <2331933560@qq.com>
2019-09-11 15:44:30 +0800

    wrote a file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld{
-   public static void main(String[]args){
-   System.out.println("Hello World");
-}
+	public static void main(String[] args){
+		sayHello(sum());
+	}
+	public static void sayHello(int sum){
+		System.out.println("Hello world !" + sum);
+	}
+	public static int sum(){
+		int sum = 0;
+		for(int i = 0;i <=100;i++){
+			sum += i;
+		}
+		return sum;
+	}
 }
