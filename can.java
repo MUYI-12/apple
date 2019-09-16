fde09b355507634e75ee4544c20277eca1086d6d
MUYI-12 <2331933560@qq.com>
2019-09-16 17:29:49 +0800

    2019-9-16

---
+++ can.java
@@ -0,0 +1,14 @@
+public class can{
+	public static void main(String[] args){
+		int math = 30 + 40;
+		System.out.printf("34 + 30 = "   + math);
+		
+		int count = 10;
+		count *= count + 2;
+		count /= 100;
+		count++;
+		
+		System.out.println("\ncount = " + count);
+		
+	}
+}
