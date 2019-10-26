8db026b94d9e44576ab27ecd041c1f9a3b079038
MUYI-12 <54808918+MUYI-12@users.noreply.github.com>
2019-10-26 11:29:50 +0800

    Delete ArrayBinarySearch.java

--- ArrayBinarySearch.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class ArrayBinarySearch{
-	public static void main(String[] args){
-		int a[]={3,15,40,67,70,83},i;
-		i=Arrays.binarySearch(a,70);
-		System.out.println(i);
-	}
-}
